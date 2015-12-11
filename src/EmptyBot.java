2511d1280f5800610a434e651cd71ddac5f61214
Michael Gloegl <m.gloegl@posteo.de>
2015-12-11 03:21:49 +0100

    JROBOTS-1 Initial Commit of Framework

---
+++ src/EmptyBot.java
@@ -0,0 +1,19 @@
+import jrobots.utils.*;
+import jrobots.simulation.simulationObjects.JRobot2015_2;
+
+/** A minimal functional Bot, ready for being programmed. 
+ * <p>Please enter the documentation via the method <tt>actions()</tt>.
+ * <p>Please rename uniquely. The individual name will be displayed in the GUI.
+ * @see JRobot2015_2#actions()
+ */
+public class EmptyBot extends JRobot2015_2 {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void actions() {
+		// TODO implement brain
+		
+		
+	}
+
+}
